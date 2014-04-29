f6cb61d2993ce195d2c60c5dd39d1de34aa11321
Maxime <max.bacoux@gmail.com>
2014-04-29 05:28:50 +0200

    fix

--- doc/search/all_b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['y',['Y',['../classngl_1_1_vector2.html#a40d7fb3416f8914df63a4424897ebb99',1,'ngl::Vector2']]]
-];
--- doc/search/functions_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['x',['X',['../classngl_1_1_vector2.html#a8bdeb4f144768bde71225f28282ff562',1,'ngl::Vector2']]]
-];
