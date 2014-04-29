6a24c3acef3d2eed4e7d58bd135fc0c73471764e
Nax <max.bacoux@gmail.com>
2014-04-29 05:39:39 +0200

    Doc

---
+++ search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['normalize',['Normalize',['../classngl_1_1_vector2.html#a3af71d5702b7b404174cd031b3577e63',1,'ngl::Vector2']]],
+  ['null',['Null',['../classngl_1_1_vector2.html#a07c0666dea25cc6ad54ebab18a9cfed6',1,'ngl::Vector2']]]
+];
---
+++ search/all_8.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['tan',['Tan',['../classngl_1_1_angle.html#aef0005dd0c7ee3716bb71e8c6276066f',1,'ngl::Angle']]],
+  ['todegrees',['toDegrees',['../classngl_1_1_angle.html#acaed259230bf5bb3e4a8531a902d3fe9',1,'ngl::Angle']]],
+  ['toradians',['toRadians',['../classngl_1_1_angle.html#a0e98f80687af461d288489a9a6aca335',1,'ngl::Angle']]],
+  ['toturns',['toTurns',['../classngl_1_1_angle.html#a8b86e71c4a052614c188ed32e2918780',1,'ngl::Angle']]],
+  ['turns',['Turns',['../classngl_1_1_angle.html#a6cbe37e9fb8553aee41828f484590419',1,'ngl::Angle']]]
+];
