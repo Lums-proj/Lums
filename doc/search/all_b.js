ff4d98b23a0cd0201604d8a268b80583a37fda3a
Nax <max.bacoux@gmail.com>
2014-04-29 05:19:28 +0200

    Updated the doc

---
+++ doc/search/all_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['y',['Y',['../classngl_1_1_vector2.html#a40d7fb3416f8914df63a4424897ebb99',1,'ngl::Vector2']]]
+];
---
+++ doc/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['vector2',['Vector2',['../classngl_1_1_vector2.html',1,'ngl']]]
+];
