9067405b813e1982035a7995cec2c7045e1790e7
Nax <max.bacoux@gmail.com>
2014-04-29 05:28:50 +0200

    fix

--- doc/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['degrees',['Degrees',['../classngl_1_1_angle.html#a0f2c46ddb06497a8755a781aee1af0ad',1,'ngl::Angle']]],
-  ['dist',['Dist',['../classngl_1_1_vector2.html#a127da5c86089fb9ec3a182e41a038f61',1,'ngl::Vector2']]],
-  ['dot',['Dot',['../classngl_1_1_vector2.html#a9ac9b5b6c87567e149cba0d4e426b8c7',1,'ngl::Vector2']]]
-];
--- doc/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['vector2',['Vector2',['../classngl_1_1_vector2.html',1,'ngl']]]
-];
