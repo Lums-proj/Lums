58c546eaf06df60f8ca212e4a96455affe768826
Nax <max.bacoux@gmail.com>
2014-11-27 08:50:14 +0100

    Fixed lm::resourcePath() when not in a bundle on OS X

--- src/OperatingSystem/MacOSX.mm
+++ src/OperatingSystem/MacOSX.mm
@@ -21,9 +21,11 @@ namespace lm
     const std::string&
     resourcePath()
     {
-        if (res_path != "")
+        if (!res_path.empty())
             return res_path;
         res_path = [[[NSBundle mainBundle] resourcePath] cStringUsingEncoding:NSASCIIStringEncoding];
+        if (res_path.empty())
+            res_path = ".";
         res_path += "/";
         return res_path;
     }
