71ceb525e07294ce8e4ec274d59a153f228a3cca
Nax <max.bacoux@gmail.com>
2015-02-05 13:38:01 +0100

    Font support

--- src/Font.cpp
+++ src/Font.cpp
@@ -17,7 +17,6 @@
 #include FT_FREETYPE_H
 #include <Lums/Font.hpp>
 #include <Lums/OperatingSystem.hpp>
-#include <iostream>
 
 #define T_WIDTH 1024
 
@@ -71,17 +70,22 @@ Font::loadFile(const std::string& filename, float size, bool resource)
             {
                 unsigned char byte = *(static_cast<unsigned char*>(face->glyph->bitmap.buffer) + j * face->glyph->bitmap.pitch + k);
 
-                std::memset(buffer + (y + j) * 4 * T_WIDTH + x + k, byte, 4);
+                std::memset(buffer + (y + j) * 4 * T_WIDTH + (x + k) * 4, byte, 4);
             }
         }
         _width[i] = face->glyph->bitmap.width;
         _height[i] = face->glyph->bitmap.rows;
         _x[i] = x;
         _y[i] = y;
+        _left[i] = face->glyph->bitmap_left;
+        _top[i] = face->glyph->bitmap_top;
+        _advance[i] = face->glyph->advance.x;
         if (face->glyph->bitmap.rows > maxY)
             maxY = face->glyph->bitmap.rows;
         x += face->glyph->bitmap.width;
     }
+    glPushAttrib(GL_ENABLE_BIT);
+    glEnable(GL_TEXTURE_2D);
     glGenTextures(1, &_texture);
     glBindTexture(GL_TEXTURE_2D, _texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
@@ -90,14 +94,17 @@ Font::loadFile(const std::string& filename, float size, bool resource)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1024, 1024, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
     delete [] buffer;
+    glPopAttrib();
 }
 
 void
 Font::load(const std::string& font, float size)
 {
     std::string filename;
-#ifdef __APPLE__
+#if defined(__APPLE__)
     filename = std::string("/Library/Fonts/") + font;
+#elif defined(WIN32) || defined(WIN64)
+    filename = std::string("/Windows/Fonts/") + font;
 #endif
     loadFile(filename, size, false);
 }
@@ -112,63 +119,55 @@ void
 Font::puts(const char* str, int x, int y) const
 {
     std::vector<GLdouble> v;
+    std::vector<GLdouble> t;
 
+    y += _top['T'];
+
+    glPushAttrib(GL_ENABLE_BIT);
+    glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, _texture);
     v.reserve(std::strlen(str) * 16);
     for (size_t j = 0; str[j]; j++)
     {
         char i = str[j];
 
-        v.push_back(x);
-        v.push_back(y);
-        v.push_back(_x[i] / 1024.0);
-        v.push_back(_y[i] / 1024.0);
-
-        v.push_back(x + _width[i]);
-        v.push_back(y);
-        v.push_back((_x[i] + _width[i]) / 1024.0);
-        v.push_back(_y[i] / 1024.0);
-
-        v.push_back(x + _width[i]);
-        v.push_back(y + _height[i]);
-        v.push_back((_x[i] + _width[i]) / 1024.0);
-        v.push_back((_y[i] + _height[i]) / 1024.0);
+        x += _left[i];
+        int ny = y - _top[i];
 
         v.push_back(x);
-        v.push_back(y + _height[i]);
-        v.push_back(_x[i] / 1024.0);
-        v.push_back((_y[i] + _height[i]) / 1024.0);
+        v.push_back(ny);
+        t.push_back(_x[i] / 1024.0);
+        t.push_back(_y[i] / 1024.0);
 
-        x += _width[i];
+        v.push_back(x + _width[i]);
+        v.push_back(ny);
+        t.push_back((_x[i] + _width[i]) / 1024.0);
+        t.push_back(_y[i] / 1024.0);
 
-        std::cout << "Letter : " << i << " dim " << _width[i] << ' ' << _height[i] << ' ' << _x[i] << ' ' << _y[i] << '\n'; 
+        v.push_back(x + _width[i]);
+        v.push_back(ny + _height[i]);
+        t.push_back((_x[i] + _width[i]) / 1024.0);
+        t.push_back((_y[i] + _height[i]) / 1024.0);
+
+        v.push_back(x);
+        v.push_back(ny + _height[i]);
+        t.push_back(_x[i] / 1024.0);
+        t.push_back((_y[i] + _height[i]) / 1024.0);
+
+        x += (_advance[i] >> 6);
     }
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    glVertexPointer(2, GL_DOUBLE, sizeof(GLdouble) * 2, &(v[0]));
-    glTexCoordPointer(2, GL_DOUBLE, sizeof(GLdouble) * 2, &(v[2]));
+    glVertexPointer(2, GL_DOUBLE, 0, &(v[0]));
+    glTexCoordPointer(2, GL_DOUBLE, 0 * 2, &(t[0]));
     glDrawArrays(GL_QUADS, 0, std::strlen(str) * 4);
     glDisableClientState(GL_TEXTURE_COORD_ARRAY);
     glDisableClientState(GL_VERTEX_ARRAY);
-}
-
-void
-Font::debug() const
-{
-    glBindTexture(GL_TEXTURE_2D, _texture);
-    glBegin(GL_QUADS);
-    glTexCoord2d(0, 0);
-    glVertex2i(0, 0);
-    glTexCoord2d(1, 0);
-    glVertex2i(200, 0);
-    glTexCoord2d(1, 1);
-    glVertex2i(200, 200);
-    glTexCoord2d(0, 1);
-    glVertex2i(0, 200);
-    glEnd();
+    glPopAttrib();
 }
 
 Font::~Font()
 {
 
 }
+
