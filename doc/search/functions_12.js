9cd43c957fe4592175d58114f31bc34167d70431
Nax <max.bacoux@gmail.com>
2015-02-12 18:42:49 +0100

    Better doc

--- doc/search/functions_12.js
+++ doc/search/functions_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['_7efont',['~Font',['../classlm_1_1_font.html#a134aaa2f78af0c12d3ce504957169768',1,'lm::Font']]],
   ['_7egamestate',['~GameState',['../classlm_1_1_game_state.html#ae623df5042cd0c17daa3394fdcb397b3',1,'lm::GameState']]],
   ['_7eimage',['~Image',['../classlm_1_1_image.html#a0294f63700543e11c0f0da85601c7ae5',1,'lm::Image']]],
   ['_7ewindow',['~Window',['../classlm_1_1_window.html#a85946945aab810cbd6bfa5b46b2adee5',1,'lm::Window']]]
--- include/Lums/Font.hpp
+++ include/Lums/Font.hpp
@@ -20,16 +20,69 @@
 
 namespace lm
 {
+    /**
+     * The font class is an abstraction over the freeType library.
+     * It provides hardware accelerated text rendering through OpenGL.
+     */
     class Font
     {
     public:
+        /**
+         * Create an empty font.
+         */
         LUMS_EXPORTED Font();
+
         LUMS_EXPORTED Font(const Font&) = delete;
+
+        /**
+         * Load a font file.
+         * After a call to this method, the font can be used for rendering.
+         * @param filename A path to the font file
+         * @size The size of the font, in dots.
+         * @resource Wether to look for the font in the resource folder. Defaults to true.
+         */
         LUMS_EXPORTED void    loadFile(const std::string& filename, float size, bool resource = true);
+
+        /**
+         * Load a system font file.
+         * Be warned, some systems may not have system fonts.
+         * You are supposed to try multiple fonts, to be sure to match at least one.
+         * @font The font name, including extension.
+         * @size The font size, in dots.
+         */
         LUMS_EXPORTED void    load(const std::string& font, float size);
+
+        /**
+         * Render text using the font.
+         * In debug mode, an error is reported if the font cannot be used.
+         * In release mode, trying to render text using a null font is undefined
+         * behavior.
+         * @param x Where to render text, x coord.
+         * @param y Where to render text, y coord.
+         * @param str The text to be rendered.
+         */
         LUMS_EXPORTED void    puts(int x, int y, const std::string& str) const;
+
+        /**
+         * This is the same as Font::puts, overloaded for const char*.
+         * @param x Where to render text, x coord.
+         * @param y Where to render text, y coord.
+         * @param str The text to be rendered.
+         */
         LUMS_EXPORTED void    puts(int x, int y, const char* str) const;
+
+        /**
+         * Render formated text. This function is equivalent to the libc printf.
+         * This function behaves like Font::puts, except for the format.
+         * @param x Where to render text, x coord.
+         * @param y Where to render text, y coord.
+         * @param format A printf-compatible format string.
+         */
         LUMS_EXPORTED void    printf(int x, int y, const char* format, ...) const;
+
+        /**
+         * Destructor
+         */
         LUMS_EXPORTED ~Font();
 
     private:
