2df060587c2f1efee9b7f3842838253fecdb6d23
Maxime BACOUX <mbacoux@e2r13p3.42.fr>
2015-03-03 18:26:05 +0100

    Templateception

--- include/Lums/Lums.hpp
+++ include/Lums/Lums.hpp
@@ -32,6 +32,7 @@
 #endif
 
 #include <Lums/Angle.hpp>
+#include <Lums/Batch2.hpp>
 #include <Lums/Core.hpp>
 #include <Lums/Event.hpp>
 #include <Lums/Font.hpp>
---
+++ include/Lums/Vertex.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*    Vertex.hpp                                     oooooo       oooooo      */
+/*                                                 oooooooooo   oooooooooo    */
+/*                                                         o%%%%%o            */
+/*                                                         %:::::%            */
+/*                                                        %:::::::%           */
+/*    This file is part of the                             %:::::%            */
+/*    Lums library.                                         %%%%%             */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LUMS_VERTEX_HPP
+#define LUMS_VERTEX_HPP
+
+namespace lm
+{
+	enum class Vertex
+	{
+		Texture,
+		Color,
+		Normal
+	};
+}
+
+#endif
