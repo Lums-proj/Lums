3ebd201b7aa41c421db0778f9f5610ff2123161f
Nax <max.bacoux@gmail.com>
2015-05-18 15:53:01 +0200

    music stop/pause ok

--- include/LumsInclude/Audio/Music.hpp
+++ include/LumsInclude/Audio/Music.hpp
@@ -14,6 +14,7 @@
 #ifndef LUMS_MUSIC_HPP
 #define LUMS_MUSIC_HPP
 
+#include  <mutex>
 #include <LumsInclude/Audio/Sound.hpp>
 
 #define NB_BUFFERS  3
@@ -23,14 +24,27 @@ namespace lm
     class Music : public Sound
     {
     public:
+
+        enum State
+        {
+            Playing,
+            Paused,
+            Stopped
+        };
+
         Music();
         void    play(Vector3f pos = {0.f, 0.f, 0.f});
         void    pause();
         void    stop();
         ~Music();
     private:
+        void    state(Music::State state, std::function<void (void)> func);
         void    streamOGG(Vector3f pos);
         void    bufferizeOGG(ALuint& buffer);
+
+        ALuint          _source;
+        Music::State    _state;
+        std::mutex      _mtx;
     };
 }
 
