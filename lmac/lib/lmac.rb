4a1a1a98da6dd922032fd4fd6126ebdc72e721fd
Maxime <max.bacoux@gmail.com>
2015-05-28 04:43:28 +0200

    lmac - support help and init

--- lmac/lib/lmac.rb
+++ lmac/lib/lmac.rb
@@ -1,1 +1,23 @@
-puts "Hello, World!"
+require File.dirname(__FILE__) + '/lmac-init'
+require File.dirname(__FILE__) + '/lmac-help'
+
+module Lmac
+  class Main
+    ACTIONS = {
+      'help' => ::Lmac::HelpCmd,
+      'init' => ::Lmac::InitCmd
+    }
+
+    def run! arg
+      arg ||= 'run'
+      action = ACTIONS[arg]
+      if action.nil?
+        puts "Invalid action: #{arg}"
+      else
+        action.new.run!
+      end
+    end
+  end
+end
+
+Lmac::Main.new.run! ARGV.first
