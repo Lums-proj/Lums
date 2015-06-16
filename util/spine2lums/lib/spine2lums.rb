8a1da34011a560623dc3888d1691650a5aacc9a8
spuyet <spuyet@student.42.fr>
2015-06-16 18:10:14 +0200

    parsing skins + atlas

--- util/spine2lums/lib/spine2lums.rb
+++ util/spine2lums/lib/spine2lums.rb
@@ -1,22 +1,46 @@
 require 'json'
+require 'awesome_print'
 
 class Spiner
-  def initialize file
-    root = JSON.parse(File.read file)
+  def initialize spine, atlas
+    @atlas = read_atlas atlas
+    root = JSON.parse(File.read spine)
     read_bones root
+    read_skins root
   end
 
-  def run! file
+  def run! spine, atlas
     puts "Bones"
-    p @bones
+    ap @bones
+    ap @skins 
     buffer = ""
     write_bones buffer
-    f = File.open file, 'wb'
+    f = File.open spine, 'wb'
     f.write buffer
     f.close
   end
 
   private
+
+  def read_atlas file
+    buffer = File.read file
+    array = buffer.split "\n"
+    header = array.shift 6
+    size = header[2].split(' ')[1].split(',').map(&:to_i)
+    atlas = []
+    array.each_slice 7 do |b|
+      hash = {}
+      hash[:name] = b[0]
+      hash[:pos] = []
+      hash[:pos][0] = b[2].split(' ')[1].to_f / size[0].to_f
+      hash[:pos][1] = b[2].split(' ')[2].to_f / size[1].to_f
+      hash[:size] = []
+      hash[:size][0] = b[3].split(' ')[1].to_f / size[0].to_f
+      hash[:size][1] = b[3].split(' ')[2].to_f / size[1].to_f      
+      atlas << hash
+    end
+    {size: size, atlas: atlas}
+  end
 
   def read_bones root
     @bones = []
@@ -37,11 +61,31 @@ class Spiner
     buffer << [@bones.size].pack('L<')
     @bones.each {|b| buffer << b.pack('Z*L<FFFFF')}
   end
+
+  def read_skins root
+    @skins = []
+    skin = {}
+    root['skins'].each_value {|s| skin.merge! s}
+    ap skin
+    skin.each do |key, value|
+      bone = root['slots'].find{|h| h['name'] == key }['bone']
+      bone_id = @bones.find_index {|b| b[0] == bone}
+      value.each do |key, value|
+        name = value['name'] || key
+        texture = @atlas[:atlas].find_index{|h| h[:name] == name}
+        x = value['x'].to_f
+        y = value['y'].to_f
+        rotation = value['rotation'].to_f
+        @skins << [texture, bone_id, x, y, rotation]
+      end
+    end
+  end
+
 end
 
-if ARGV.size != 2
-  puts "usage: spine2lums <spine.json> <output>"
+if ARGV.size != 4
+  puts "usage: spine2lums <spine.json> <spine.atlas> <anim.bin> <atlas.bin>"
 else
-  spiner = Spiner.new ARGV[0]
-  spiner.run! ARGV[1]
+  spiner = Spiner.new ARGV[0], ARGV[1]
+  spiner.run! ARGV[2], ARGV[3] 
 end
