e2df1156cb668d2d262ee444074b27442753ef40
Nax <max.bacoux@gmail.com>
2015-07-19 19:50:13 +0200

    Fixed bench

--- benchmark/PoolAlloc.cpp
+++ benchmark/PoolAlloc.cpp
@@ -40,22 +40,22 @@ Benchmark::poolAlloc()
     for (int i = 0; i < 79; ++i)
         bar[i] = '*';
     puts(bar);
-    timeReport("std::allocator alloc then dealloc (10k, 100B)", checkAlloc<std::allocator, 10000, 100>);
-    timeReport("std::allocator alloc then dealloc (50k, 100B)", checkAlloc<std::allocator, 50000, 100>);
+    timeReport("std::allocator alloc then dealloc (10k,  100B)", checkAlloc<std::allocator, 10000, 100>);
+    timeReport("std::allocator alloc then dealloc (50k,  100B)", checkAlloc<std::allocator, 50000, 100>);
     timeReport("std::allocator alloc then dealloc (200k, 100B)", checkAlloc<std::allocator, 200000, 100>);
-    timeReport("std::allocator alloc then dealloc (1M, 100B)", checkAlloc<std::allocator, 100000, 100>);
-    timeReport("std::allocator alloc then dealloc (10k, 10k)", checkAlloc<std::allocator, 10000, 10000>);
-    timeReport("std::allocator alloc then dealloc (50k, 10k)", checkAlloc<std::allocator, 50000, 10000>);
-    timeReport("std::allocator alloc then dealloc (200k, 10k)", checkAlloc<std::allocator, 200000, 10000>);
-    timeReport("std::allocator alloc then dealloc (1M, 10k)", checkAlloc<std::allocator, 1000000, 10000>);
+    timeReport("std::allocator alloc then dealloc (1M,   100B)", checkAlloc<std::allocator, 100000, 100>);
+    timeReport("std::allocator alloc then dealloc (10k,  10k )", checkAlloc<std::allocator, 10000, 10000>);
+    timeReport("std::allocator alloc then dealloc (50k,  10k )", checkAlloc<std::allocator, 50000, 10000>);
+    timeReport("std::allocator alloc then dealloc (200k, 10k )", checkAlloc<std::allocator, 200000, 10000>);
+    timeReport("std::allocator alloc then dealloc (1M,   10k )", checkAlloc<std::allocator, 1000000, 10000>);
     puts(bar);
-    timeReport("lm::PoolAllocator alloc then dealloc (10k, 100B)", checkAlloc<lm::PoolAllocator, 10000, 100>);
-    timeReport("lm::PoolAllocator alloc then dealloc (50k, 100B)", checkAlloc<lm::PoolAllocator, 50000, 100>);
+    timeReport("lm::PoolAllocator alloc then dealloc (10k,  100B)", checkAlloc<lm::PoolAllocator, 10000, 100>);
+    timeReport("lm::PoolAllocator alloc then dealloc (50k,  100B)", checkAlloc<lm::PoolAllocator, 50000, 100>);
     timeReport("lm::PoolAllocator alloc then dealloc (200k, 100B)", checkAlloc<lm::PoolAllocator, 200000, 100>);
-    timeReport("lm::PoolAllocator alloc then dealloc (1M, 100B)", checkAlloc<lm::PoolAllocator, 100000, 100>);
-    timeReport("lm::PoolAllocator alloc then dealloc (10k, 10k)", checkAlloc<lm::PoolAllocator, 10000, 10000>);
-    timeReport("lm::PoolAllocator alloc then dealloc (50k, 10k)", checkAlloc<lm::PoolAllocator, 50000, 10000>);
-    timeReport("lm::PoolAllocator alloc then dealloc (200k, 10k)", checkAlloc<lm::PoolAllocator, 200000, 10000>);
-    timeReport("lm::PoolAllocator alloc then dealloc (1M, 10k)", checkAlloc<lm::PoolAllocator, 1000000, 10000>);
+    timeReport("lm::PoolAllocator alloc then dealloc (1M,   100B)", checkAlloc<lm::PoolAllocator, 100000, 100>);
+    timeReport("lm::PoolAllocator alloc then dealloc (10k,  10k )", checkAlloc<lm::PoolAllocator, 10000, 10000>);
+    timeReport("lm::PoolAllocator alloc then dealloc (50k,  10k )", checkAlloc<lm::PoolAllocator, 50000, 10000>);
+    timeReport("lm::PoolAllocator alloc then dealloc (200k, 10k )", checkAlloc<lm::PoolAllocator, 200000, 10000>);
+    timeReport("lm::PoolAllocator alloc then dealloc (1M,   10k )", checkAlloc<lm::PoolAllocator, 1000000, 10000>);
     puts(bar);
 }
